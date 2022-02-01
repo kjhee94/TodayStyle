b4a45cb732ec885acc6c7ca2610c084c0c5d17c7
unknown <shjhyeonji1@naver.com>
2022-02-01 14:56:17 +0900

    공지 디비연결

--- src/main/java/kr/or/iei/board/controller/BoardController.java
+++ src/main/java/kr/or/iei/board/controller/BoardController.java
@@ -1,22 +1,35 @@
 package kr.or.iei.board.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-import kr.or.iei.member.model.service.MemberService;
+import kr.or.iei.board.model.service.BoardService;
+import kr.or.iei.board.model.vo.FAQ;
+import kr.or.iei.board.model.vo.Notice;
+
 
 @Controller
 public class BoardController {
 	
 	@Autowired
-	private MemberService mService;
+	private BoardService bService;
 	
 	@RequestMapping(value="/board/boardPage.do", method=RequestMethod.GET)
-	public String boardPage()
+	public ModelAndView boardPage(ModelAndView mav)
 	{
-		return "board/boardPage";
+		ArrayList<FAQ> list = bService.AllFAQ();
+		ArrayList<Notice> nlist = bService.AllNotice();
+		System.out.println(nlist);
+		mav.addObject("list", list);
+		mav.addObject("nlist", nlist);
+		mav.setViewName("board/boardPage"); 
+		return mav;
+		
 	}
 	
 	@RequestMapping(value="/board/detailPage.do", method=RequestMethod.GET)
---
+++ src/main/java/kr/or/iei/board/model/vo/FAQ.java
@@ -0,0 +1,84 @@
+package kr.or.iei.board.model.vo;
+
+import java.sql.Date;
+
+public class FAQ {
+	private int faqNo;
+	private String category;
+	private String title;
+	private String content;
+	private String userId;
+	private Date regdate;
+	private char endYN;
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "FAQ [faqNo=" + faqNo + ", category=" + category + ", title=" + title + ", content=" + content
+				+ ", userId=" + userId + ", regdate=" + regdate + ", endYN=" + endYN + "]";
+	}
+	public FAQ(int faqNo, String category, String title, String content, String userId, Date regdate, char endYN) {
+		super();
+		this.faqNo = faqNo;
+		this.category = category;
+		this.title = title;
+		this.content = content;
+		this.userId = userId;
+		this.regdate = regdate;
+		this.endYN = endYN;
+	}
+	public FAQ() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getFaqNo() {
+		return faqNo;
+	}
+	public void setFaqNo(int faqNo) {
+		this.faqNo = faqNo;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public char getEndYN() {
+		return endYN;
+	}
+	public void setEndYN(char endYN) {
+		this.endYN = endYN;
+	}
+	
+	
+	
+	
+	
+	
+}
