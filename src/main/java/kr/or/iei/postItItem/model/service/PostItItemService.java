ba67fa3fb85396210a14f1de5bd451be360f2563
kjh <kjhee_94@naver.com>
2022-02-14 20:44:25 +0900

    2022년 02월 14일 / 잇템 개별 페이지 데이터 불러오기 완(사진 제외)

--- src/main/java/kr/or/iei/postItItem/model/service/PostItItemService.java
+++ src/main/java/kr/or/iei/postItItem/model/service/PostItItemService.java
@@ -1,5 +1,12 @@
 package kr.or.iei.postItItem.model.service;
 
+import java.util.HashMap;
+
+import org.springframework.stereotype.Service;
+
+import kr.or.iei.postItItem.model.vo.PostItItem;
+
 public interface PostItItemService {
 
+	HashMap<String, Object> oneItItemPost(int itItemNo);
 }
