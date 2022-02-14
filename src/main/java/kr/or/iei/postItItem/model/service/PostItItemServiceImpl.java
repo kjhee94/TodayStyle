27d7990a83e93f362813ed9ab2d18c7ecf7d7fd9
kjh <kjhee_94@naver.com>
2022-02-14 15:17:36 +0900

    220214 오류 수정

--- src/main/java/kr/or/iei/postItItem/model/service/PostItItemServiceImpl.java
+++ src/main/java/kr/or/iei/postItItem/model/service/PostItItemServiceImpl.java
@@ -1,22 +1,6 @@
 package kr.or.iei.postItItem.model.service;
 
-<<<<<<< HEAD
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import kr.or.iei.postCoordi.model.dao.BoardDto;
-import kr.or.iei.postCoordi.model.vo.PostCoordi;
-import kr.or.iei.postItItem.model.dao.PostItItemDAO;
-import kr.or.iei.postItItem.model.vo.PostItItem;
-
-
-@Service
-public class PostItItemServiceImpl {
-=======
 public class PostItItemServiceImpl implements PostItItemService{
->>>>>>> 423878f7c604342dc3471feb94bc48051d513742
-
-	
 
 }
  
